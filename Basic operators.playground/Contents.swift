b10cc64c52f4c42b87838880d726cd0e0f38adb2
Maksim Tsarevich <maksim.tsarevich@gmail.com>
2022-08-09 12:09:42 +0300

    Second day str

--- Basic operators.playground/Contents.swift
+++ Basic operators.playground/Contents.swift
@@ -1,3 +1,128 @@
-import UIKit
+// Операторы присваивания
+let b = 10
+var a = 5
+a = b
 
-var greeting = "Hello, playground"
+let (x, y) = (1, 2)
+x
+y
+
+// Арифметические операторы
+let sum = 1 + 3
+let sub = 5 - 3
+let mult = 2 * 4
+let div = 10.0 / 2.4
+
+let welcomeString = "Hello, " + "world"
+
+// Оператор целочисленного деления
+
+let c = 10 % 4 // 2
+
+// Оператор унарного минуса
+
+let three = 3
+let newThree = -three // -3
+let plusThree = -newThree // 3
+
+// Оператор унарного плюса
+let minusSix = -6
+let plusSix = +minusSix // -6
+
+// Составные операторы присваивания
+var g = 1
+g += 2 // 3
+
+// Операторы сравнения
+three == plusThree // true
+three != newThree // true
+three > newThree // true
+three < sub // false
+three >= sub // true
+three <= mult // true
+
+let name = "Hello"
+if name == "Hello"{
+    print("Hello world")
+} else {
+    print("Error")
+}
+
+(1, "z") < (2, "a")
+(3, "a") < (3, "b")
+(5, "d") == (5, "d")
+
+// Тернарный условный оператор
+let contentHeight = 40
+let hasHeader = true
+//let rowHeight = contentHeight + (hasHeader ? 50 : 20)
+var rowHeight = contentHeight
+
+if hasHeader{
+    rowHeight += 50
+} else {
+    rowHeight += 20
+}
+
+// Оператор объединения по nil
+let defaultColorName = "red"
+var userDefinedColorName: String?
+var colorNameToUse = userDefinedColorName ?? defaultColorName
+
+// Оператор замкнутого диапазона
+
+for index in 1...5 {
+    print("\(index) mult 5 = \(index * 5)")
+}
+
+// Оператор полузамкнутого диапазона
+let names = ["Max", "Ilya", "Fedya", "X"]
+let count = names.count
+
+for i in 0..<count {
+    print("\(i + 1) names \(names[i])")
+}
+
+// Односторонние диапазоны
+for name in names[2...] {
+    print(name)
+}
+
+for name in names[...2] {
+    print(name)
+}
+
+for name in names[..<2] {
+    print(name)
+}
+
+// Оператор логического НЕ
+let allowewEntry = false
+if !allowewEntry{
+    print("Error")
+}
+
+// Оператор логического И
+let enteredDoorCode = true
+let passedRetinaScan = false
+
+if enteredDoorCode && passedRetinaScan{
+    print("Yes")
+} else {
+    print("Noooooo")
+}
+
+// Оператор логического ИЛИ
+
+if enteredDoorCode || passedRetinaScan{
+    print("Yes")
+} else {
+    print("Nooooo")
+}
+
+// Комбинирование логических операторов
+if allowewEntry && passedRetinaScan || enteredDoorCode {
+    print("Yeeeeeeees")
+} else {
+    print("No please repeate ")
+}
--- Strings and symbols.playground/Contents.swift
+++ Strings and symbols.playground/Contents.swift
@@ -1,3 +1,133 @@
-import UIKit
+// Строковые литералы
+let someString = "Some stirng literal value"
 
-var greeting = "Hello, playground"
+//  Специальные символы в строковых лиетралах
+let wiseWords = "\"Blablabla\" - Eisads"
+let dollarSign = "\u{24}"
+let blackHeart = "\u{2665}"
+let sparklingHeart = "\u{1F496}"
+
+// Расширенные разделители строк
+let threeMoreDouble = #"""
+here are three more double: """
+gfsdfds
+dfsdffds
+"""#
+print(threeMoreDouble)
+
+// Инициализация пустых строк
+var emptyString = ""
+var anotherEmptyString = String()
+
+if emptyString.isEmpty {
+    print("String is empty")
+}
+
+// Изменчивость строк
+var variableString = "Horse"
+variableString += " Hello"
+
+// Работа с символами
+for character in "Dog!" {
+    print(character)
+}
+
+let exclamationMark: Character = "!"
+
+let catCharacter: [Character] = ["c", "a", "t", "s", "!"]
+let catString = String(catCharacter)
+print(catString)
+
+// Конкатенация строк и символов
+let string1 = "Hello"
+let string2 = " World"
+var welcome = string1 + string2
+welcome += " ,i am Maks"
+
+welcome.append(exclamationMark)
+
+// Интерполяция строк
+let multiplier = 3
+let message = "\(multiplier) times 2.5 is \(Double(multiplier) * 2.5)"
+
+// Подсчет символов
+let unusualMenagerie = "Car, Dog, Chiken, Muha"
+
+print("unusualMenagerie: \(unusualMenagerie.count) symbols")
+
+// Индексы строки
+let greeting = "Guten Tag!"
+greeting[greeting.index(before: greeting.endIndex)]
+greeting[greeting.index(after: greeting.startIndex)]
+greeting[greeting.startIndex]
+greeting[greeting.index(greeting.startIndex, offsetBy: 7)]
+
+for index in greeting.indices {
+    print("\(greeting[index])", terminator: " ")
+}
+
+// Добавление и удаление
+ var welcomeString = "hello"
+welcomeString.insert("!", at: welcomeString.endIndex)
+
+welcomeString.insert(contentsOf: " Three", at: welcomeString.index(before: welcomeString.endIndex))
+
+welcomeString.remove(at: welcomeString.index(before: welcomeString.endIndex))
+welcomeString
+
+let range = welcomeString.index(welcomeString.endIndex, offsetBy: -6) ..< welcomeString.endIndex
+welcomeString.removeSubrange(range)
+welcomeString
+
+// Подстроки
+
+let greetin = "Hello, world"
+let index = greetin.firstIndex(of: ",") ?? greetin.endIndex
+let begginning = greetin[..<index]
+let newString = String(begginning)
+
+// Сравнение строк
+
+let quatation = "hello, my name is"
+let newQuatation = "hello, my name is"
+
+if quatation == newQuatation{
+    print("\nString uqual")
+}
+
+// Равенство префиксов и суффиксов
+let romeoAndJuliet = [
+"Act 1 Scene 1: Grodno",
+"Act 1 Scene 2: Minsk",
+"Act 1 Scene 3: Mogilev",
+"Act 1 Scene 4: Brest",
+"Act 2 Scene 1: Minsk"
+]
+
+var act1SceneCount = 0
+for scene in romeoAndJuliet{
+    if scene.hasPrefix("Act 1"){
+        act1SceneCount += 1
+    }
+}
+
+print("All scene in act 1 is: \(act1SceneCount)")
+
+
+var grodnoCount = 0
+var minskCount = 0
+
+for scene in romeoAndJuliet{
+    if scene.hasSuffix("Minsk"){
+        minskCount += 1
+    } else if scene.hasSuffix("Grodno"){
+        grodnoCount += 1
+    }
+}
+
+print("Scene in place Grodno: \(grodnoCount), and scene in place Minsk: \(minskCount)")
+
+
+
+
+
